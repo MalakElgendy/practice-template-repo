b6171265aaadd5dd0c1e8daddcbd25fe9b562294
malak <malkmoelgendy@gmail.com>
2025-02-14 20:19:01 +0200

    third

---
+++ index.js
@@ -0,0 +1,10 @@
+function movePlayer(direction){
+    return new Promise((resolve) => {
+        resolve()
+    }).then(data => {
+        setTimeout(()=>{
+            console.log(direction)
+        },2000)
+    });
+}
+movePlayer('up')
